458560ad815cbd8987f25b3c06bef244b78f7da4
Brianna Escobar <briannaescobar6@gmail.com>
2023-04-22 17:50:22 -0400

    Lifting-state

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState();
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1> Lifting Up State </h1>
+
+       {!user && <><section style= {{ border: '2px Solid red', width: '100%', padding: '16px'}}>
+        <Signup setUser={setUser}/>
+       </section>
+
+       <section style= {{ border: '2px Solid yellow', width: '100%', padding: '16px'}}>
+        <Login setUser={setUser}/>
+       </section></>}
+
+      {user && <section style= {{ border: '2px Solid blue', width: '100%', padding: '16px'}}>
+        <Dashboard user={user} setUser={setUser} />
+       </section>}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
